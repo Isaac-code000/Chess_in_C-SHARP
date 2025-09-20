1583333ea743f7cb20388e0b5d24c2dc681831be
Isaac-code <isaac.cade26@gmail.com>
2025-09-19 23:51:08 -0300

    King Moves

--- Chess/ChessRules/King.cs
+++ Chess/ChessRules/King.cs
@@ -15,7 +15,47 @@ namespace ChessRules
 
         public override string ToString()
         {
-            return "K"; 
+            return "K";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece p = Board.GetPiece(position);
+            return p == null || p.Color != Color;
+        }
+
+        private void TestMove(Position position, bool[,] mat)
+        {
+            if (Board.ValidatePosition(position) && CanMove(position))
+            {
+                mat[position.Lines, position.Columns] = true;
+            }
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Lines, Board.Columns];
+            Position pos = new Position(0, 0);
+
+            pos.SetValues(Position.Lines - 1, Position.Columns - 1);
+            TestMove(pos, mat);
+            pos.SetValues(Position.Lines - 1, Position.Columns);
+            TestMove(pos, mat);
+            pos.SetValues(Position.Lines - 1, Position.Columns + 1);
+            TestMove(pos, mat);
+
+            pos.SetValues(Position.Lines, Position.Columns - 1);
+            TestMove(pos, mat);
+            pos.SetValues(Position.Lines, Position.Columns + 1);
+            TestMove(pos, mat);
+
+            pos.SetValues(Position.Lines + 1, Position.Columns + 1);
+            TestMove(pos, mat);
+            pos.SetValues(Position.Lines + 1, Position.Columns);
+            TestMove(pos, mat);
+            pos.SetValues(Position.Lines + 1, Position.Columns - 1);
+            TestMove(pos, mat);
+            return mat;
         }
     }
 }
