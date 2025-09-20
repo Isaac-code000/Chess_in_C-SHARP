8c3659e233faca4d6f5745c755f498255c124b94
Isaac-code <isaac.cade26@gmail.com>
2025-09-20 01:38:50 -0300

    Queen Moves

--- Chess/ChessRules/Queen.cs
+++ Chess/ChessRules/Queen.cs
@@ -12,6 +12,95 @@ namespace ChessRules
         public Queen(Colors color, Board board) : base(color, board)
         {
         }
+
+        private bool CanMove(Position position)
+        {
+            Piece p = Board.GetPiece(position);
+            return p == null || p.Color != Color;
+        }
+
+        private void TestMove(Position position, bool[,] mat)
+        {
+            if (Board.ValidatePosition(position) && CanMove(position))
+            {
+                mat[position.Lines, position.Columns] = true;
+            }
+        }
+
+        private void DiagonalMoves(Position pos, bool[,] mat, char op1, char op2)
+        {
+            while (true)
+            {
+                TestMove(pos, mat);
+                if (Board.ThereIsAPiece(pos) && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                if (op1 == '+' && op2 == '+')
+                    pos.SetValues(pos.Lines + 1, pos.Columns + 1);
+                else if (op1 == '+' && op2 == '-')
+                    pos.SetValues(pos.Lines + 1, pos.Columns - 1);
+                else if (op1 == '-' && op2 == '+')
+                    pos.SetValues(pos.Lines - 1, pos.Columns + 1);
+                else
+                    pos.SetValues(pos.Lines - 1, pos.Columns - 1);
+            }
+        }
+
+        private void LineMoves(Position pos, bool[,] mat, char op)
+        {
+            while (true)
+            {
+                TestMove(pos, mat);
+                if (Board.ThereIsAPiece(pos) && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                if (op == '+')
+                    pos.SetValues(pos.Lines + 1, pos.Columns);
+                else
+                    pos.SetValues(pos.Lines - 1, pos.Columns);
+            }
+        }
+
+        private void ColumnMoves(Position pos, bool[,] mat, char op)
+        {
+            while (true)
+            {
+                TestMove(pos, mat);
+                if (Board.ThereIsAPiece(pos) && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                if (op == '+')
+                    pos.SetValues(pos.Lines, pos.Columns + 1);
+                else
+                    pos.SetValues(pos.Lines, pos.Columns - 1);
+            }
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Lines, Board.Columns];
+            Position pos = new Position(0, 0);
+            pos.SetValues(pos.Lines + 1, pos.Columns + 1);
+            DiagonalMoves(pos, mat, '+', '+');
+            pos.SetValues(pos.Lines - 1, pos.Columns - 1);
+            DiagonalMoves(pos, mat, '-', '-');
+            pos.SetValues(pos.Lines + 1, pos.Columns - 1);
+            DiagonalMoves(pos, mat, '+', '-');
+            pos.SetValues(pos.Lines - 1, pos.Columns + 1);
+            DiagonalMoves(pos, mat, '-', '+');
+            pos.SetValues(Position.Lines - 1, Position.Columns);
+            LineMoves(pos, mat, '-');
+            pos.SetValues(Position.Lines + 1, Position.Columns);
+            LineMoves(pos, mat, '+');
+            pos.SetValues(Position.Lines, Position.Columns - 1);
+            ColumnMoves(pos, mat, '-');
+            pos.SetValues(Position.Lines, Position.Columns + 1);
+            ColumnMoves(pos, mat, '+');
+            return mat;
+        }
         public override string ToString()
         {
             return "Q";
