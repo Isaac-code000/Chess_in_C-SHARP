fa5a00cf2be68c2422394a8fb7551e1ef7daacee
Isaac-code <isaac.cade26@gmail.com>
2025-09-20 01:09:35 -0300

    Tower Moves

--- Chess/ChessRules/Tower.cs
+++ Chess/ChessRules/Tower.cs
@@ -7,8 +7,10 @@ using ChessBoard;
 
 namespace ChessRules
 {
+
     internal class Tower : Piece
     {
+        
         public Tower(Colors color,Board board) : base(color, board)
         {
         }
@@ -16,6 +18,67 @@ namespace ChessRules
         public override string ToString()
         {
             return "T";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece p = Board.GetPiece(position);
+            return p == null || p.Color != Color;
+        }
+
+        private void TestMove(Position position, bool[,] mat)
+        {
+            if (Board.ValidatePosition(position) && CanMove(position))
+            {
+                mat[position.Lines, position.Columns] = true;
+            }
+        }
+
+        private void LineMoves(Position pos, bool[,] mat,char op)
+        {
+            while (true)
+            {
+                TestMove(pos, mat);
+                if (Board.ThereIsAPiece(pos) && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                if(op == '+')
+                    pos.SetValues(pos.Lines + 1, pos.Columns);
+                else
+                    pos.SetValues(pos.Lines - 1, pos.Columns);
+            }
+        }
+
+        private void ColumnMoves(Position pos, bool[,] mat,char op)
+        {
+            while (true)
+            {
+                TestMove(pos, mat);
+                if (Board.ThereIsAPiece(pos) && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                if(op == '+')
+                    pos.SetValues(pos.Lines, pos.Columns + 1);
+                else
+                    pos.SetValues(pos.Lines, pos.Columns - 1);
+            }
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Lines, Board.Columns];
+            Position pos = new Position(0, 0);
+            pos.SetValues(Position.Lines - 1, Position.Columns);
+            LineMoves(pos, mat, '-');
+            pos.SetValues(Position.Lines + 1, Position.Columns);
+            LineMoves(pos, mat, '+');
+            pos.SetValues(Position.Lines, Position.Columns - 1);
+            ColumnMoves(pos, mat, '-'); 
+            pos.SetValues(Position.Lines, Position.Columns + 1);
+            ColumnMoves(pos, mat, '+');
+            return mat;
         }
     }
 }
