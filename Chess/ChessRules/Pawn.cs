c3b11ce2ae16d5110b7aaef999d34fc7c7c8efeb
Isaac-code <isaac.cade26@gmail.com>
2025-09-23 19:38:22 -0300

    Promotion

--- Chess/ChessRules/Pawn.cs
+++ Chess/ChessRules/Pawn.cs
@@ -11,6 +11,7 @@ namespace ChessRules
     {
         public Pawn(Colors color, Board board) : base(color, board)
         {
+            Name = 'P';
         }
 
         private bool canMove(Position position)
@@ -21,10 +22,49 @@ namespace ChessRules
 
         private bool TestPosition(Position position, bool[,] mat)
         {
-            if(Board.ValidPosition(position) && canMove(position)){
+            if (Board.ValidPosition(position) && canMove(position))
+            {
                 mat[position.Lines, position.Columns] = true;
             }
             return true;
+        }
+
+        private void Promotion(Piece p)
+        {
+            Position aux = this.Position;
+            p.MoveCount = MoveCount;
+            Board.RemovePiece(Position);
+            Board.PutPiece(p, aux);
+        }
+
+        private void ChosenPromotion()
+        {
+            Console.WriteLine("Chosen the promotion of pawn");
+            Console.WriteLine("{ Q } { T } { B } { H }");
+            char op = char.Parse(Console.ReadLine().ToUpper());
+
+            switch (op)
+            {
+                case 'Q':Promotion(new Queen(Color, Board)); break;
+                case 'T':Promotion(new Tower(Color, Board)); break;
+                case 'H':Promotion(new Horse(Color, Board)); break;
+                case 'B':Promotion(new Bishop(Color, Board)); break;
+            }
+        }
+
+        public void ExecutePromotion()
+        {
+            ChosenPromotion();
+        }
+
+
+        private void EnPassant()
+        {
+            if (Position.Lines == 3 && Color == Colors.white)
+            {
+                Position left = new Position(Position.Lines, Position.Columns - 1);
+                Position right = new Position(Position.Lines, Position.Columns + 1);
+            }
         }
 
         public override bool[,] PossibleMoves()
@@ -35,24 +75,24 @@ namespace ChessRules
             if (Board.GetPiece(GetPosition()).Color == Colors.white)
             {
                 pos.SetValues(Position.Lines - 1, Position.Columns);
-                if(Board.GetPiece(pos) == null)
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) == null)
                 {
                     mat[pos.Lines, pos.Columns] = true;
                 }
-               
+
                 pos.SetValues(Position.Lines - 2, Position.Columns);
-                if (MoveCount == 0 && Board.GetPiece(pos) == null)
+                if (Board.ValidPosition(pos) && MoveCount == 0 && Board.GetPiece(pos) == null)
                 {
                     TestPosition(pos, mat);
                 }
 
                 pos.SetValues(Position.Lines - 1, Position.Columns - 1);
-                if (Board.GetPiece(pos) != null)
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) != null)
                 {
                     TestPosition(pos, mat);
                 }
                 pos.SetValues(Position.Lines - 1, Position.Columns + 1);
-                if (Board.GetPiece(pos) != null)
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) != null)
                 {
                     TestPosition(pos, mat);
                 }
@@ -60,19 +100,24 @@ namespace ChessRules
             else
             {
                 pos.SetValues(Position.Lines + 1, Position.Columns);
-                TestPosition(pos, mat);
-                if (MoveCount == 0)
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) == null)
                 {
-                    pos.SetValues(Position.Lines + 2, Position.Columns);
+                    mat[pos.Lines, pos.Columns] = true;
+                }
+
+                pos.SetValues(Position.Lines + 2, Position.Columns);
+                if (Board.ValidPosition(pos) && MoveCount == 0 && Board.GetPiece(pos) == null)
+                {
+                    TestPosition(pos, mat);
+                }
+
+                pos.SetValues(Position.Lines + 1, Position.Columns + 1);
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) != null)
+                {
                     TestPosition(pos, mat);
                 }
                 pos.SetValues(Position.Lines + 1, Position.Columns - 1);
-                if (Board.GetPiece(pos) != null)
-                {
-                    TestPosition(pos, mat);
-                }
-                pos.SetValues(Position.Lines + 1, Position.Columns + 1);
-                if (Board.GetPiece(pos) != null)
+                if (Board.ValidPosition(pos) && Board.GetPiece(pos) != null)
                 {
                     TestPosition(pos, mat);
                 }
--- Chess/ChessRules/PlayMatch.cs
+++ Chess/ChessRules/PlayMatch.cs
@@ -16,11 +16,15 @@ namespace ChessRules
         public Colors CurrentPlayer { get; private set; }
         public int Turn { get; private set; }
 
+        public HashSet<Piece> CapturedPieces;
+        public HashSet<Piece> PiecesInGame;
+
         public PlayMatch()
         {
             Turn = 1;
             CurrentPlayer = Colors.white;
-            Layout.SetLayout(Board);
+            PiecesInGame = new HashSet<Piece>();
+            CapturedPieces = new HashSet<Piece>();
         }
 
         public void NewTurn()
@@ -75,9 +79,17 @@ namespace ChessRules
                     }
                     else
                     {
+                        
                         Board.RemovePiece(destiny);
                         Board.PutPiece(Board.GetPiece(origin), destiny);
+                        CapturedPieces.Add(Board.GetPiece(destiny));    
                         Board.RemovePiece(origin);
+                        if (Board.GetPiece(origin).Name == 'P' && destiny.Lines == 0 || destiny.Lines == 7)
+                        {
+                            Pawn aux = (Pawn)Board.GetPiece(destiny);
+                            aux.ExecutePromotion();
+                        }
+
                         Board.GetPiece(destiny).IncreaseMoveCount();
                     }
                 }
@@ -90,10 +102,70 @@ namespace ChessRules
             }
         }
 
-        public bool XequeMate()
+        public void PutNewPiece(char column, int line, Piece piece)
         {
-            return true;
+            Board.PutPiece(piece, new Position(line - 1, column - 'a'));
+            PiecesInGame.Add(piece);
         }
 
+        public void SetLayout(Board board)
+        {
+
+            PutNewPiece('a', 1, new Tower(Colors.black, board));
+            PutNewPiece('b', 1, new Horse(Colors.black, board));
+            PutNewPiece('c', 1, new Bishop(Colors.black, board));
+            PutNewPiece('d', 1, new Queen(Colors.black, board));
+            PutNewPiece('e', 1, new King(Colors.black, board));
+            PutNewPiece('f', 1, new Bishop(Colors.black, board));
+            PutNewPiece('g', 1, new Horse(Colors.black, board));
+            PutNewPiece('h', 1, new Tower(Colors.black, board));
+
+            for (int i = 0; i < 7; i++)
+            {
+            PutNewPiece((char)('a' + i), 2, new Pawn(Colors.black, board));
+            PutNewPiece((char)('e' + i), 7, new Pawn(Colors.white, board));
+            }
+            // Setting up white pieces
+            PutNewPiece('a', 8, new Tower(Colors.white, board));
+            PutNewPiece('b', 8, new Horse(Colors.white, board));
+            PutNewPiece('c', 8, new Bishop(Colors.white, board));
+            PutNewPiece('d', 8, new Queen(Colors.white, board));
+            PutNewPiece('e', 8, new King(Colors.white, board));
+            PutNewPiece('f', 8, new Bishop(Colors.white, board));
+            PutNewPiece('g', 8, new Horse(Colors.white, board));
+            PutNewPiece('h', 8, new Tower(Colors.white, board));
+        }
+
+        public HashSet<Piece> CapturedPiecesByColor(Colors color)
+        {
+            HashSet<Piece> aux = new HashSet<Piece>();
+            foreach (Piece piece in CapturedPieces)
+            {
+                if (piece.Color == color)
+                {
+                    aux.Add(piece);
+                }
+            }
+            return aux;
+        }
+
+        public HashSet<Piece> PiecesInGameByColor(Colors color)
+        {
+            HashSet<Piece> aux = new HashSet<Piece>();
+            foreach (Piece piece in PiecesInGame)
+            {
+                if (piece.Color == color)
+                {
+                    aux.Add(piece);
+                }
+            }
+            aux.ExceptWith(CapturedPiecesByColor(color));
+            return aux;
+        }   
+
+        public bool CheckMate()
+        {
+            return true;
+        }   
     }
 }
